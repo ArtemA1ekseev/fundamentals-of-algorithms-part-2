0eaba4fa6a4b02336c9a06f89ee0a53650624086
ArtemA1ekseev <a1ekseevartem.ru@gmail.com>
2022-11-26 18:58:55 +0400

    Initial commit.

---
+++ src/main/java/com/example/fundamentalsofalgorithms/FundamentalsOfAlgorithmsPart2Application.java
@@ -0,0 +1,13 @@
+package com.example.fundamentalsofalgorithms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentalsOfAlgorithmsPart2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FundamentalsOfAlgorithmsPart2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fundamentalsofalgorithms/FundamentalsOfAlgorithmsPart2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fundamentalsofalgorithms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentalsOfAlgorithmsPart2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
